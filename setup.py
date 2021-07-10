e1bee90eaeed2b95de1fd1f7684847d4b27c0475
Ricardo J González C <rjgcastillo@hotmail.com>
2021-07-10 16:29:36 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='threaded_cv2',
+    version='0.1.0',
+    description='A package that contains threaded CV2 io classes implementations',
+    url='https://github.com/ricglz/threaded_cv2',
+    author='Ricardo Gonzalez',
+    author_email='rjgcastillo2@gmail.com',
+    license='MIT License',
+    packages=['pyexample'],
+    install_requires=[
+        'cv2',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Visualization'
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Typing :: Typed'
+    ],
+)
